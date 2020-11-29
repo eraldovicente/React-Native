b49add2f482ba5b80e9a866353fe59c71385be94
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-29 21:24:41 -0200

    Hola Mundo en React Native

---
+++ citas/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text style={styles.encabezado}>Hola Mundo!</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  encabezado: {
+    textAlign: 'center',
+    marginTop: 100
+  }
+});
+
+export default App;
