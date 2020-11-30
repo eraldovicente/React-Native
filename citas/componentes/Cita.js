c8f0a803d3e5e3df1f22178879134eb369ff1f91
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-30 01:19:57 -0200

    Estilos al componente de <Cita>

--- citas/componentes/Cita.js
+++ citas/componentes/Cita.js
@@ -3,22 +3,41 @@ import { Text, StyleSheet, View } from 'react-native';
 
 const Cita = ({item}) => {
      return ( 
-          <View>
+          <View style={styles.cita}>
                <View>
-                    <Text>Paciente: </Text>
-                    <Text>{item.paciente}</Text>
+                    <Text style={styles.label}>Paciente: </Text>
+                    <Text style={styles.texto}>{item.paciente}</Text>
                </View>
                <View>
-                    <Text>Propietario: </Text>
-                    <Text>{item.propietario}</Text>
+                    <Text style={styles.label}>Propietario: </Text>
+                    <Text style={styles.texto}>{item.propietario}</Text>
                </View>
                <View>
-                    <Text>Síntomas: </Text>
-                    <Text>{item.sintomas}</Text>
+                    <Text style={styles.label}>Síntomas: </Text>
+                    <Text style={styles.texto}>{item.sintomas}</Text>
                </View>
           </View>
                
       );
 }
+
+const styles = StyleSheet.create({
+     cita: {
+          backgroundColor: '#FFF',
+          borderBottomColor: '#e1e1e1',
+          borderStyle: 'solid',
+          borderBottomWidth: 1,
+          paddingVertical: 20,
+          paddingHorizontal: 10
+     },
+     label: {
+          fontWeight: 'bold',
+          fontSize: 18,
+          marginTop: 20
+     },
+     texto: {
+          fontSize: 18
+     }
+})
  
 export default Cita;
