5a3c2c2b78d62ee6bf6378977e3cf96ad33ca617
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-03 02:25:29 -0200

    Agregando una cuadricula de imagenes

--- layoutsnative/App.js
+++ layoutsnative/App.js
@@ -72,6 +72,38 @@ const App = () => {
                 />
               </View>
           </View>
+
+          <Text style={styles.titulo}>Hospedajes en LA</Text>
+
+          <View
+            style={styles.listado}
+          >
+              <View style={styles.listadoItem}>
+                <Image 
+                    style={styles.mejores}
+                    source={ require('./assets/img/hospedaje1.jpg') }
+                />
+              </View>
+              <View style={styles.listadoItem}>
+                <Image 
+                    style={styles.mejores}
+                    source={ require('./assets/img/hospedaje2.jpg') }
+                />
+              </View>
+              <View style={styles.listadoItem}>
+                <Image 
+                    style={styles.mejores}
+                    source={ require('./assets/img/hospedaje3.jpg') }
+                />
+              </View>
+              <View style={styles.listadoItem}>
+                <Image 
+                    style={styles.mejores}
+                    source={ require('./assets/img/hospedaje4.jpg') }
+                />
+              </View>
+          </View>
+
         </View>
       </ScrollView>
     </>
@@ -100,6 +132,14 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 200,
     marginVertical: 5
+  },
+  listado: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  listadoItem: {
+    flexBasis: '49%'
   }
 });
 
