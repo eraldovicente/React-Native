dd6097f5032475e8c1fd41714c97fca0d86faa83
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-02 23:44:41 -0200

    Creando el proyecto y primeros ajustes

---
+++ layoutsnative/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
