6713f8a99944ffd64fd6a768f16b3097febb48db
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-03 00:16:52 -0200

    Factor de crescimiento con flex

--- layoutsnative/App.js
+++ layoutsnative/App.js
@@ -22,18 +22,22 @@ const styles = StyleSheet.create({
   caja1: {
     padding: 20,
     backgroundColor: 'navy',
+    flex: 1
   },
   caja2: {
     padding: 20,
     backgroundColor: 'yellow',
+    flex: 1
   },
   caja3: {
     padding: 20,
     backgroundColor: 'green',
+    flex: 1
   },
   caja4: {
     padding: 20,
     backgroundColor: 'teal',
+    flex: 3
   }
 });
 
