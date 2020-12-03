ea77db6fc151122f7181b3e75a3c5acae0850dbf
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-03 02:11:11 -0200

    Agregando un listado de imagenes

--- layoutsnative/App.js
+++ layoutsnative/App.js
@@ -4,53 +4,76 @@ import { StyleSheet, View, Image, Text, ScrollView } from 'react-native';
 const App = () => {
   return (
     <>
-      <View style={{ flexDirection: 'row' }}>
+      <ScrollView>      
+        <View style={{ flexDirection: 'row' }}>
 
-        <Image
-          style={styles.banner}
-          source={ require('./assets/img/bg.jpg') }
-        />
+          <Image
+            style={styles.banner}
+            source={ require('./assets/img/bg.jpg') }
+          />
 
-      </View> 
+        </View> 
 
-      <View style={styles.contenedor}>
-        <Text style={styles.titulo}>Qué hacer en Paris</Text>
-        <ScrollView
-          horizontal
-        >
-          <View>
-            <Image 
-                style={styles.ciudad}
-                source={ require('./assets/img/actividad1.jpg') }
-            />
-          </View>
-          <View>
-            <Image 
-                style={styles.ciudad}
-                source={ require('./assets/img/actividad2.jpg') }
-            />
-          </View>
-          <View>
-            <Image 
-                style={styles.ciudad}
-                source={ require('./assets/img/actividad3.jpg') }
-            />
-          </View>
-          <View>
-            <Image 
-                style={styles.ciudad}
-                source={ require('./assets/img/actividad4.jpg') }
-            />
-          </View>
-          <View>
-            <Image 
-                style={styles.ciudad}
-                source={ require('./assets/img/actividad5.jpg') }
-            />
-          </View>
-        </ScrollView>
-      </View>
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Qué hacer en Paris</Text>
+          <ScrollView
+            horizontal
+          >
+            <View>
+              <Image 
+                  style={styles.ciudad}
+                  source={ require('./assets/img/actividad1.jpg') }
+              />
+            </View>
+            <View>
+              <Image 
+                  style={styles.ciudad}
+                  source={ require('./assets/img/actividad2.jpg') }
+              />
+            </View>
+            <View>
+              <Image 
+                  style={styles.ciudad}
+                  source={ require('./assets/img/actividad3.jpg') }
+              />
+            </View>
+            <View>
+              <Image 
+                  style={styles.ciudad}
+                  source={ require('./assets/img/actividad4.jpg') }
+              />
+            </View>
+            <View>
+              <Image 
+                  style={styles.ciudad}
+                  source={ require('./assets/img/actividad5.jpg') }
+              />
+            </View>
+          </ScrollView>
 
+          <Text style={styles.titulo}>Mejores Alojamientos</Text>
+          <View>
+            <View>
+                <Image 
+                    style={styles.mejores}
+                    source={ require('./assets/img/mejores1.jpg') }
+                />
+              </View>
+              <View>
+                <Image 
+                    style={styles.mejores}
+                    source={ require('./assets/img/mejores2.jpg') }
+                />
+              </View>
+              <View>
+                <Image 
+                    style={styles.mejores}
+                    source={ require('./assets/img/mejores3.jpg') }
+                />
+              </View>
+          </View>
+        </View>
+      </ScrollView>
     </>
   );
 };
@@ -72,6 +95,11 @@ const styles = StyleSheet.create({
     width: 250,
     height: 300,
     marginRight: 10
+  }, 
+  mejores: {
+    width: '100%',
+    height: 200,
+    marginVertical: 5
   }
 });
 
