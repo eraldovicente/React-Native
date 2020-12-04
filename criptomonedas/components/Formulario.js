49d7184b4a0ce01d6c9e54825a3e430eefe18b27
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-04 15:03:14 -0200

    Leyendo el valor seleccionado del picker

--- criptomonedas/components/Formulario.js
+++ criptomonedas/components/Formulario.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 
 const Formulario = () => {
+
+     const [ moneda, guardarMoneda ] = useState('');
+     const [ criptomoneda, guardarCriptomoneda ] = useState('');
+
+     const obtenerMoneda = moneda => {
+          guardarMoneda(moneda);
+     }
+
      return ( 
 
           <View>
                <Text style={styles.label}>Moneda</Text>
-               <Picker>
+               <Picker
+                    selectedValue={moneda}
+                    onValueChange={ moneda => obtenerMoneda(moneda) }
+               >
                     <Picker.Item label="- Seleccione -" value=""/>
                     <Picker.Item label="Dolar de Estados Unidos" value="USD"/>
                     <Picker.Item label="Peso Mexicano" value="MXN"/>
