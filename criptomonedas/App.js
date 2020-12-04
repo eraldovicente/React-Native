6a08dc639dcbacc699fa4b11df8788b29547c756
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-04 02:45:59 -0200

    Mostrando una imagen del criptomonedas

--- criptomonedas/App.js
+++ criptomonedas/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
+import { StyleSheet, Image } from 'react-native';
 import Header from './components/Header';
 
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
 const App = () => {
   return (
     <>
       <Header />
+
+      <Image
+        style={styles.imagen}
+        source={ require('./assets/img/cryptomonedas.png') }
+      />
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  imagen: {
+    width: '100%',
+    height: 150,
+    // marginHorizontal: '2.5%'
+  }
 });
 
 export default App;
