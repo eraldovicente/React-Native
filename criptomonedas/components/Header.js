2c625674ad9f8b3faab04d347cd2fd5b538c0c00
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-04 02:20:26 -0200

    Finalizando el Header

--- criptomonedas/components/Header.js
+++ criptomonedas/components/Header.js
@@ -8,7 +8,14 @@ const Header = () => (
 const styles = StyleSheet.create({
      encabezado: {
           paddingTop: Platform.OS === 'ios' ? 50 : 10,
-          fontFamily: 'Lato-Black'
+          fontFamily: 'Lato-Black',
+          backgroundColor: '#5E49E2',
+          paddingBottom: 10,
+          textAlign: 'center',
+          textTransform: 'uppercase',
+          fontSize: 20,
+          color: '#FFF',
+          marginBottom: 30
      }
 })
 
