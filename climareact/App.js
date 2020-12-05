51a831073965d1e285a8f78bccdd3a680029676d
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-05 21:01:31 -0200

    Comenzando con el formulario

--- climareact/App.js
+++ climareact/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
   StatusBar,
 } from 'react-native';
+import Formulario from './components/Formulario';
 
 const App = () => {
   return (
     <>
-     
+     <Formulario />
     </>
   );
 };
---
+++ climareact/components/Formulario.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, TextInput, View, StyleSheet } from 'react-native';
+
+const Formulario = () => {
+     return (  
+          <>
+               <View style={styles.formulario}>
+                    <View>
+                         <TextInput 
+                              placeholder="Ciudad"
+                              placeholderTextColor="#666"
+                         />
+                    </View>
+               </View>
+          </>
+     );
+}
+
+const styles = StyleSheet.create({
+     formulario: {
+          marginTop: 100
+     }
+})
+ 
+export default Formulario;
