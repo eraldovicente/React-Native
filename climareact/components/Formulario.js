fe7f086de81e1dc4e41bef4c926a88dfe061a39d
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-05 21:44:38 -0200

    Agregando el Picker de opciones

--- climareact/components/Formulario.js
+++ climareact/components/Formulario.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, TextInput, View, StyleSheet } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 const Formulario = () => {
      return (  
@@ -10,6 +11,18 @@ const Formulario = () => {
                               placeholder="Ciudad"
                               placeholderTextColor="#666"
                          />
+                    </View>
+                    <View>
+                         <Picker>
+                              <Picker.Item label="-- Seleccione un país --" value="" />
+                              <Picker.Item label="Estados Unidos" value="US" />
+                              <Picker.Item label="México" value="MX" />
+                              <Picker.Item label="Argentina" value="AR" />
+                              <Picker.Item label="Colombia" value="CO" />
+                              <Picker.Item label="Costa Rica" value="CR" />
+                              <Picker.Item label="España" value="ES" />
+                              <Picker.Item label="Perú" value="PE" />
+                          </Picker>
                     </View>
                </View>
           </>
