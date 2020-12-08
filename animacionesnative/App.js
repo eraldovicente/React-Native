874717c638f8dc63ef871052fcdfb8f88d04ae02
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-08 19:47:31 -0200

    Primeros pasos y que es Animated

---
+++ animacionesnative/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Animacion1 from './components/Animacion1';
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.contenido}>
+        <Animacion1 />
+      </View>      
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenido: {
+    marginTop: 100
+  }
+});
+
+export default App;
---
+++ animacionesnative/components/Animacion1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Stylesheet, Text, Animated } from 'react-native';
+
+const Animacion1 = () => {
+     return ( 
+          <Text>Animacion 1</Text>
+     );
+}
+ 
+export default Animacion1;
