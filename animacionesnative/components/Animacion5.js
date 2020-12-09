e5c26b87003ac5adeef27d2a012b4c6f147b5add
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-09 00:13:24 -0200

    .spring

---
+++ animacionesnative/components/Animacion5.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TouchableNativeFeedback, Animated } from 'react-native';
+
+const Animacion5 = () => {
+
+     const [ animacion ] = useState( new Animated.Value(1));
+
+     const presionarBtn = () => {
+          Animated.spring( animacion, {
+               toValue: .8
+          } ).start();
+     }
+
+     const soltarBtn = () => {
+
+     }
+
+     const estiloAnimacion = {
+          transform: [{ scale: animacion }]
+     }
+
+     return ( 
+          <View style={styles.contenedor}>
+               <TouchableNativeFeedback
+                    onPressIn={ () => presionarBtn() }
+                    onPressOut={ () => soltarBtn() }
+               >
+                    <Animated.View style={[ styles.btn, estiloAnimacion ]}>
+                         <Text
+                              style={styles.texto}>Iniciar Sesión</Text>
+                    </Animated.View>
+               </TouchableNativeFeedback>
+          </View>
+      );
+}
+
+const styles = StyleSheet.create({
+     contenedor: { 
+          alignItems: 'center'
+     },
+     btn: {
+          backgroundColor: 'cornflowerblue',
+          width: 280,
+          height: 80,
+          justifyContent: 'center',
+          alignItems: 'center'
+     },
+     texto: {
+          color: '#FFF',
+          fontWeight: 'bold',
+          textTransform: 'uppercase',
+          fontSize: 28
+     }
+});
+ 
+export default Animacion5;
