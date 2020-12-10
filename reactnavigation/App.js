ab7cb243314aabc6a7ed9b2d9698254c64092fc2
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-10 21:54:28 -0200

    Que es React Navigation

---
+++ reactnavigation/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
