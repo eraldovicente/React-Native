3b9eeca0479e12ea09909bd59de724d1468e4dad
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-11 00:26:43 -0200

    Creando una navegacion y components

--- reactnavigation/App.js
+++ reactnavigation/App.js
@@ -2,10 +2,34 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { View, Text } from 'react-native';
 
+// React Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Inicio from './views/Inicio';
+import Nosotros from './views/Nosotros';
+
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
     <>
-      <Text>Navigation</Text>
+      <NavigationContainer>
+
+        <Stack.Navigator
+          initialRouteName='Inicio'
+        >
+          <Stack.Screen
+            name='Inicio'
+            component={Inicio}
+          />
+          <Stack.Screen
+            name='Nosotros'
+            component={Nosotros}
+          />
+        </Stack.Navigator>
+
+      </NavigationContainer>
     </>
   );
 };
