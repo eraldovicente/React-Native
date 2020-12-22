21e2a228169d5a89ee92f0d085f8be985803d3cc
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-22 00:13:58 -0200

    Agregando el resto de campos al formulario

--- crudnative/views/NuevoCliente.js
+++ crudnative/views/NuevoCliente.js
@@ -4,14 +4,48 @@ import { TextInput, Headline, Button } from 'react-native-paper';
 import globalStyles from '../styles/global';
 
 const NuevoCliente = () => {
+
+     const leerNombre = () => {
+          console.log('Escribiendo');
+     }
+
      return ( 
           <View style={globalStyles.contenedor}>
                <Headline style={globalStyles.titulo}>Añadir Nuevo Cliente</Headline>
 
                <TextInput
+                    label="Nombre"
+                    placeholder="Eraldo"
+                    onChangeText={ () => leerNombre() }
+                    style={styles.input}
                />
+               <TextInput
+                    label="Teléfono"
+                    placeholder="6969696969"
+                    onChangeText={ () => leerNombre() }
+                    style={styles.input}
+               />
+               <TextInput
+                    label="Correo"
+                    placeholder="correo@correo.com"
+                    onChangeText={ () => leerNombre() }
+                    style={styles.input}
+               />
+               <TextInput
+                    label="Empresa"
+                    placeholder="Nombre Empresa"
+                    onChangeText={ () => leerNombre() }
+                    style={styles.input}
+               />               
           </View>
      );
 }
- 
+
+const styles = StyleSheet.create({
+     input: {
+          marginBottom: 20,
+          backgroundColor: 'transparent'
+     }
+})
+
 export default NuevoCliente;
