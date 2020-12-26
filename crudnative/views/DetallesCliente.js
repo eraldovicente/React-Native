1192f4c89c019aa9966dde3a97eebc12bf5b2dc8
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-26 01:14:37 -0200

    Ver la informacion de un cliente

--- crudnative/views/DetallesCliente.js
+++ crudnative/views/DetallesCliente.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-const DetallesCliente = () => {
+const DetallesCliente = ({route}) => {
+
+     console.log(route.params);
      return ( 
           <Text>Desde DetallesCliente</Text>
       );
--- crudnative/views/Inicio.js
+++ crudnative/views/Inicio.js
@@ -42,6 +42,7 @@ const Inicio = ({navigation}) => {
                          <List.Item
                               title={item.nombre}
                               description={item.empresa}
+                              onPress={ () => navigation.navigate("DetallesCliente", { item })}
                          />
                     )}
                     
