5e9473ddcaf2a1542513559d6c141947d3de5fec
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-26 18:19:25 -0200

    Agregando un boton de eliminar

--- crudnative/views/DetallesCliente.js
+++ crudnative/views/DetallesCliente.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 import { Text, Headline, Subheading, Button } from 'react-native-paper';
 import globalStyles from '../styles/global';
 
 const DetallesCliente = ({route}) => {
 
      const { nombre, telefono, correo, empresa } = route.params.item;
+
+     const mostrarConfirmacion = () => {
+          Alert.alert(
+               'Deseas eliminar este cliente?',
+               'Un contacto eliminado no se puede recuperar',
+               [
+                    { text: 'Si, Eliminar', onPress: () => eliminarContacto() },
+                    { text: 'Cancelar', style: 'cacel' }
+               ]
+          )
+     }
+
+     const eliminarContacto = () => {
+          console.log('eliminando...');
+     }
 
      return ( 
           <View style={globalStyles.contenedor}>
@@ -14,7 +29,12 @@ const DetallesCliente = ({route}) => {
                <Text style={styles.texto}>Correo: <Subheading>{correo}</Subheading>  </Text>
                <Text style={styles.texto}>Teléfono: <Subheading>{telefono}</Subheading>  </Text>
 
-               <Button>
+               <Button 
+                    style={styles.boton} 
+                    mode="contained" 
+                    icon="cancel"
+                    onPress={ () => mostrarConfirmacion() }
+               >
                     Eliminar Cliente
                </Button>
           </View>
@@ -24,6 +44,10 @@ const styles = StyleSheet.create({
      texto: {
           marginBottom: 20,
           fontSize: 18
+     },
+     boton: {
+          marginTop: 100,
+          backgroundColor: 'red'
      }
 })
  
