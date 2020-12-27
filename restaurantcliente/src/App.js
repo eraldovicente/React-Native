c316b064583f6e4da182035e863b62a312fab528
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-27 21:12:07 -0200

    Creando las rutas y componentes

--- restaurantcliente/src/App.js
+++ restaurantcliente/src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Routes, Route } from 'react-router';
 
 import Ordenes from './components/paginas/Ordenes';
+import Menu from './components/paginas/Menu';
+import NuevoPlatillo from './components/paginas/NuevoPlatillo';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Ordenes />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
       </Routes>
     </div>
   );
