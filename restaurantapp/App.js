40267874446056c1d76bfa89a4bbf0ddb4463b76
Eraldo Vicente <eraldovicente2@gmail.com>
2020-12-30 03:36:45 -0200

    Agregando React Navigation

--- restaurantapp/App.js
+++ restaurantapp/App.js
@@ -1,9 +1,32 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import NuevaOrden from './views/NuevaOrden';
+import Menu from './views/Menu';
+import DetallePlatillo from './views/DetallePlatillo';
+import FormularioPlatillo from './views/FormularioPlatillo';
+import ResumenPedido from './views/ResumenPedido';
+import ProgresoPedido from './views/ProgresoPedido';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <>
-      
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NuevaOrden"
+            component={NuevaOrden}
+            options={{
+              title: "Nueva Orden"
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer> 
     </>
   );
 };
