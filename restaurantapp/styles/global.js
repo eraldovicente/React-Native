4019badb4dec9c47aac3e2d0407a65e858676385
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-03 17:43:16 -0200

    Mostrando el producto seleccionado

--- restaurantapp/styles/global.js
+++ restaurantapp/styles/global.js
@@ -15,6 +15,16 @@ const globalStyles = StyleSheet.create({
           textTransform: 'uppercase',
           fontWeight: 'bold',
           color: '#000'
+     },
+     titulo: {
+          textAlign: 'center',
+          marginTop: 40,
+          marginBottom: 20,
+          fontSize: 22
+     },
+     imagen: {
+          height: 300,
+          width: '100%'
      }
 });
 
--- restaurantapp/views/DetallePlatillo.js
+++ restaurantapp/views/DetallePlatillo.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useContext } from 'react';
+import { Image } from 'react-native';
+import {
+     Container,
+     Content,
+     Footer,
+     FooterTab,
+     Button,
+     Body,
+     Text,
+     H1,
+     Card,
+     CardItem
+} from 'native-base';
+import globalStyles from '../styles/global';
+
+import PedidoContext from '../context/pedidos/pedidosContext';
 
 const DetallePlatillo = () => {
+
+     // Pedido context
+     const { platillo } = useContext(PedidoContext);
+     const { nombre, imagen, descripcion, precio } = platillo;
+
+     console.log(platillo);
+
      return ( 
-          <Text>DetallePlatillo</Text>
+          <Container style={globalStyles.contenedor}> 
+               <Content style={globalStyles.contenido}>
+                    <H1 style={globalStyles.titulo}>{nombre}</H1>
+
+                    <Card>
+                         <CardItem>
+                              <Body>
+                                   <Image style={globalStyles.imagen} source={{ uri: imagen}} />
+                              </Body>
+                         </CardItem>
+                    </Card>
+               </Content>
+          </Container>
       );
 }
  
