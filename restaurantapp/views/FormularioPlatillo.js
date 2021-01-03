a0d0fa7e13617618238dcad22df628f58cba7361
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-03 18:37:09 -0200

    El grid de Nativebase

--- restaurantapp/views/FormularioPlatillo.js
+++ restaurantapp/views/FormularioPlatillo.js
@@ -1,9 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import {
+     Container,
+     Content,
+     Form,
+     Icon,
+     Input,
+     Grid,
+     Col,
+     Button,
+     Text
+} from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+import globalStyles from '../styles/global';
+
+import PedidoContext from '../context/pedidos/pedidosContext';
 
 const FormularioPlatillo = () => {
      return ( 
-          <Text>FormularioPlatillo</Text>
+          <Container>
+               <Content>
+                    <Form>
+                         <Text style={globalStyles.titulo}>Cantidad</Text>
+                         <Grid>
+                              <Col>
+                                   <Button>
+                                        <Icon name="remove" />
+                                   </Button>
+                              </Col>
+                              <Col>
+                              
+                              </Col>
+                              <Col>
+                                   <Button>
+                                        <Icon name="add" />
+                                   </Button>
+                              </Col>
+                         </Grid>
+                    </Form>
+               </Content>
+          </Container>
       );
 }
  
