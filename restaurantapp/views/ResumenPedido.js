88b4fe04a811ef6e4de65210f2d11e577be553f8
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-03 23:02:10 -0200

    Trabajando con el resumen del pedido

--- restaurantapp/views/ResumenPedido.js
+++ restaurantapp/views/ResumenPedido.js
@@ -1,9 +1,56 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useContext, useEffect } from 'react';
+import { StyleSheet, Alert } from 'react-native';
+import {
+     Container,
+     Content,
+     List,
+     ListItem,
+     Thumbnail,
+     Text,
+     Left,
+     Body,
+     Button,
+     H1,
+     Footer,
+     FooterTab
+} from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+import globalStyles from '../styles/global';
+
+import PedidoContext from '../context/pedidos/pedidosContext';
 
 const ResumenPedido = () => {
+
+     // context de pedido
+     const { pedido } =  useContext(PedidoContext);
+     console.log(pedido);
+
      return ( 
-          <Text>ResumenPedido</Text>
+          <Container style={globalStyles.contenedor}>
+               <Content style={globalStyles.contenido}>
+                    <H1 style={globalStyles.titulo}>Resumen Pedido</H1>
+                    {pedido.map( platillo => {
+                         const { cantidad, nombre, imagen, id, precio } = platillo;
+                         return(
+                              <List key={id}>
+                                   <ListItem thumbnail>
+                                        <Left>
+                                             <Thumbnail large square source={{ uri: imagen}} />
+                                        </Left>
+
+                                        <Body>
+                                             <Text>{nombre}</Text>
+                                             <Text>Cantidad: {cantidad}</Text>
+                                             <Text>Precio: R$ {precio}</Text>
+                                        </Body>
+                                   </ListItem>
+                              </List>
+                         )
+                    })}
+
+                    <Text style={globalStyles.cantidad}>Total a pagar: R$</Text>
+               </Content>
+          </Container>
       );
 }
  
