74cd4aeab852a57eb1591c45c28802825683b829
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-04 21:08:52 -0200

    Consulta para obtener las ordenes no completas

--- restaurantcliente/src/components/paginas/Ordenes.js
+++ restaurantcliente/src/components/paginas/Ordenes.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { FirebaseContext } from '../../firebase';
 
 const Ordenes = () => {
+
+     // context para las operaciones de firebase
+     const { firebase } = useContext(FirebaseContext);
+
+     // state con las ordenes
+     const [ordenes, guardarOrdenes] = useState([]);
+     
+     useEffect(() => {
+          const obtenerOrdenes = () => {
+               firebase.db.collection('ordenes').where('completado', '==', false).onSnapshot(manejarSnapshot);
+          }
+          obtenerOrdenes();
+     }, []);
+
+     function manejarSnapshot(snapshot) {
+          const ordenes = snapshot.docs.map(doc => {
+               return {
+                    id: doc.id,
+                    ...doc.data()
+               }
+          });
+          guardarOrdenes(ordenes);
+     }
+
      return ( 
           <>
                <h1 className="text-3xl font-light mb-4">Ordenes</h1>
