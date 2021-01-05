87a62af6f1ccbb9ac3611e2985e28c36af20a016
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-04 23:04:29 -0200

    Mostrar el tiempo de entrega en React Native

--- restaurantapp/views/ProgresoPedido.js
+++ restaurantapp/views/ProgresoPedido.js
@@ -4,12 +4,27 @@ import { Container, Text, H1, H3, Button} from 'native-base';
 import globalStyles from '../styles/global';
 import { useNavigation } from '@react-navigation/native';
 import PedidoContext from '../context/pedidos/pedidosContext';
+import firebase from '../firebase';
 
 const ProgresoPedido = () => {
 
      const { idpedido } = useContext(PedidoContext);
+
+     const [ tiempo, guardarTiempo ] = useState(0);
+
+     useEffect(() => {
+          const obtenerProducto = () => {
+               firebase.db.collection('ordenes')
+                    .doc(idpedido)
+                    .onSnapshot(function(doc) {
+                         guardarTiempo(doc.data().tiempoentrega);
+                    })
+          }
+          obtenerProducto()
+     }, []);
+
      return ( 
-          <Text>{idpedido}</Text>
+          <Text>{tiempo}</Text> 
       );
 }
  
