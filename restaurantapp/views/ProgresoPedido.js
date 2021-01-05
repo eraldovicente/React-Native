f79776ddd7bb0ea04e4ff85191aa4513d2b6f0f5
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-05 00:02:20 -0200

    Completando el componente tiempo de entrega

--- restaurantapp/views/ProgresoPedido.js
+++ restaurantapp/views/ProgresoPedido.js
@@ -5,6 +5,7 @@ import globalStyles from '../styles/global';
 import { useNavigation } from '@react-navigation/native';
 import PedidoContext from '../context/pedidos/pedidosContext';
 import firebase from '../firebase';
+import Countdown from 'react-countdown';
 
 const ProgresoPedido = () => {
 
@@ -23,9 +24,47 @@ const ProgresoPedido = () => {
           obtenerProducto()
      }, []);
 
+     // Muestra el countdown en la pantalla
+     const renderer = ({minutes, seconds}) => {
+ 
+          return (
+               <Text style={styles.tiempo}>{minutes}:{seconds}</Text>
+          )
+     };
+
      return ( 
-          <Text>{tiempo}</Text> 
+          <Container style={globalStyles.contenedor}>
+               <View style={[ globalStyles.contenido, { marginTop: 50 }]}>
+                    { tiempo === 0 && (
+                         <>
+                              <Text style={{ textAlign: 'center'}}>Hemos recibido tu orden...</Text>
+                              <Text style={{ textAlign: 'center'}}>Estamos calculando el tiempo de entrega</Text>
+                         </>
+                    )}
+
+                    { tiempo > 0 && (
+                         <>
+                              <Text style={{ textAlign: 'center'}}>Su orden estará lista en: </Text> 
+                              
+                              <Countdown
+                                   date={ Date.now() + tiempo * 60000 }
+                                   renderer={renderer}
+                              />
+                              
+                         </>
+                    )}
+               </View>
+          </Container>
       );
 }
- 
+
+const styles = StyleSheet.create({
+     tiempo: {
+          marginBottom: 20,
+          fontSize: 60,
+          textAlign: 'center',
+          marginTop: 30
+     }
+})
+
 export default ProgresoPedido;
