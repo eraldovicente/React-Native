0a665cde3ed9ffbe192e8758f7e514bb0a0ac424
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-04 22:36:47 -0200

    Definir tiempo de entrega React Web

--- restaurantcliente/src/components/ui/Orden.js
+++ restaurantcliente/src/components/ui/Orden.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { FirebaseContext } from '../../firebase';
 
 const Orden = ({ orden }) => {
-     console.log(orden.orden);
+
+     const [tiempoentrega, guardarTiempoEntrega ] = useState(0);
+
+     // Context de firebase
+     const { firebase } = useContext(FirebaseContext);
+
+     // define el tiempo de entrega en tiempo real
+     const definirTiempo = id => {
+          try {
+               firebase.db.collection('ordenes')     
+                    .doc(id)
+                    .update({
+                         tiempoentrega
+                    })
+          } catch (error) {
+               console.log(error);      
+          }
+     }
+
      return ( 
           <div className="sm:w-1/2 lg:w-1/3 px-2 mb-4">
                <div className="p-3 shadow-md bg-white">
@@ -23,9 +42,12 @@ const Orden = ({ orden }) => {
                                    min="1"
                                    max="20"
                                    placeholder="20"
+                                   value={tiempoentrega}
+                                   onChange={ e => guardarTiempoEntrega( parseInt(e.target.value)) }
                               />
 
                               <button
+                                   onClick={ () => definirTiempo(orden.id) }
                                    type="submit"
                                    className='bg-gray-800 hover:bg-gray-900 w-full mt-5 p-2 text-white uppercase font-bold'
                               >
