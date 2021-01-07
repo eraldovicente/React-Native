ea8d391fca18cba076f47d34f2425de329ba1ed1
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-07 01:05:11 -0200

    Almacenando el usuario autenticado en el proyecto

--- upTaskGraphQL/db/resolvers.js
+++ upTaskGraphQL/db/resolvers.js
@@ -66,10 +66,13 @@ const resolvers = {
                }
 
           },
-          nuevoProyecto: async (_, {input}) => {
+          nuevoProyecto: async (_, {input}, ctx) => {
                
                try {
                     const proyecto = new Proyecto(input);
+
+                    // asociar el creador
+                    proyecto.creador = ctx.usuario.id;
 
                     // almacenarlo en la BD
                     const resultado = await proyecto.save();
--- upTaskGraphQL/index.js
+++ upTaskGraphQL/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server');
-
+const jwt = require('jsonwebtoken');
+require('dotenv').config('variables.env');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
 
@@ -8,7 +9,23 @@ const conectarDB = require('./config/db');
 // Conectar a la BD
 conectarDB(); 
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+     typeDefs, 
+     resolvers,
+     context: ({req}) => {
+          const token = req.headers['authorization'] || '';
+          if (token) {
+               try {
+                    const usuario = jwt.verify(token, process.env.SECRETA);
+                    return {
+                         usuario
+                    }
+               } catch (error) {
+                    console.log(error);
+               }
+          }
+     } 
+});
 
 server.listen().then( ({url}) => {
      console.log(`Servidor listo en la URL ${url}`);
