55b26f77585939a53b145bb2002100bf121fcc1a
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-08 00:34:06 -0200

    Creando el proyecto y primeros pasos

---
+++ upTaskNative/views/CrearCuenta.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native'
+
+const CrearCuenta = () => {
+     return ( 
+          <Text>CrearCuenta</Text>
+      );
+}
+ 
+export default CrearCuenta;
