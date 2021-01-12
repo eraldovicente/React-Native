7c2dc52470b6c2b01f19b434ed0faf96916e55b7
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-12 20:32:01 -0200

    Validacion del formulario

--- upTaskNative/views/Proyecto.js
+++ upTaskNative/views/Proyecto.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Button, Text, H2, Content, List, Form, Item, Input, Toast } from 'native-base';
 import globalStyles from '../styles/global';
 
 const Proyecto = ({route}) => {
+
+     // state del componente
+     const [ nombre, guardarNombre ] = useState('');
+     const [ mensaje, guardarMensaje ] = useState(null);
+
+     // Validar y crear tareas
+     const handleSubmit = () => {
+          if(nombre === '') {
+               guardarMensaje('El nombre de la tarea es obligatorio');
+               return;
+          }
+
+
+          // Almacenarlo en la base de datos
+     }
+
+     const mostrarAlerta = () => {
+          Toast.show({
+               text: mensaje,
+               buttonText: 'OK',
+               duration: 5000
+          })
+     }
+
      return ( 
           <Container style={[globalStyles.contenedor], { backgroundColor: '#e84347'}}>
                <Form style={{ marginHorizontal: '2.5%', marginTop: 20 }}>
                     <Item inlineLabel last style={globalStyles.input}>
                          <Input 
                               placeholder="Nombre Tarea"
+                              value={nombre}
+                              onChangeText={ texto => guardarNombre(texto) }
                          />
                     </Item>
 
@@ -16,10 +42,12 @@ const Proyecto = ({route}) => {
                          style={globalStyles.boton}
                          square
                          block
+                         onPress={ () => handleSubmit() }
                     >
                          <Text>Crear Tarea</Text>
                     </Button>
                </Form>
+               {mensaje && mostrarAlerta() }
           </Container>
       );
 }
