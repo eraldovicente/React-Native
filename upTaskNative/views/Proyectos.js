9b807cdea0bfb00fe20fe701e1310bddf61f5858
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-12 03:48:26 -0200

    Consultar para obtener los proyectos

--- upTaskNative/views/Proyectos.js
+++ upTaskNative/views/Proyectos.js
@@ -3,10 +3,23 @@ import { StyleSheet } from 'react-native';
 import { Container, Button, Text, H2, Content, List, ListItem, Left, Right } from 'native-base';
 import globalStyles from '../styles/global';
 import { useNavigation } from '@react-navigation/native';
+import { gql, useQuery } from '@apollo/client';
+
+const OBTENER_PROYECTOS = gql`
+     query obtenerProyectos {
+          obtenerProyectos {
+               id
+               nombre
+          }
+     }
+`;
 
 const Proyectos = () => {
 
      const navigation = useNavigation();
+
+     // Apollo
+     const { data, loading, error} = useQuery(OBTENER_PROYECTOS);
 
      return ( 
           <Container style={[ globalStyles.contenedor, {backgroundColor: '#E84347'}]}>
